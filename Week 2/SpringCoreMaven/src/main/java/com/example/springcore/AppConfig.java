3d950fd0daec7a853db8335ac4942163f4adb63d
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-06 17:29:49 +0530

    Add files via upload

---
+++ Week 2/SpringCoreMaven/src/main/java/com/example/springcore/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.springcore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Student student() {
+        return new Student();
+    }
+
+}
---
+++ Week 2/SpringCoreMaven/src/main/java/com/example/springcore/Student.java
@@ -0,0 +1,12 @@
+package com.example.springcore;
+
+public class Student {
+
+    public Student() {
+        System.out.println("Student Bean Created");
+    }
+
+    public void display() {
+        System.out.println("Welcome to Spring Core!");
+    }
+}
