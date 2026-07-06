fe1bb1da562dd537098343d7bbaf806de305c7ba
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-06 21:56:49 +0530

    Add files via upload

---
+++ Week 2/MavenProjectDemo/src/main/java/com/example/mavenproject/HelloMaven.java
@@ -0,0 +1,8 @@
+package com.example.mavenproject;
+
+public class HelloMaven {
+
+    public void display() {
+        System.out.println("Welcome to Maven Project!");
+    }
+}
---
+++ Week 2/MavenProjectDemo/src/main/java/com/example/mavenproject/Main.java
@@ -0,0 +1,11 @@
+package com.example.mavenproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HelloMaven hello = new HelloMaven();
+
+        hello.display();
+    }
+}
