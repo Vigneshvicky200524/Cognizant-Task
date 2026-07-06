fce1ad2ba2e5814e6cc888e75c8c8e538ee6b5b3
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-06 16:55:45 +0530

    Add files via upload

---
+++ Week 1/SLF4JLoggingProject/src/main/java/com/example/SLF4JLoggingProject/LoggingExample.java
@@ -0,0 +1,20 @@
+package com.example.SLF4JLoggingProject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Application Started");
+
+        logger.warn("Low Memory Warning!");
+
+        logger.error("Database Connection Failed!");
+
+        logger.info("Application Finished");
+    }
+}
