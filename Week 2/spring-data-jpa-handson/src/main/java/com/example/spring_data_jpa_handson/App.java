4acd23d1a64fb21c20bcdec5712baa5be890a92f
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-06 22:25:41 +0530

    Add files via upload

---
+++ Week 2/spring-data-jpa-handson/src/main/java/com/example/spring_data_jpa_handson/App.java
@@ -0,0 +1,10 @@
+package com.example.spring_data_jpa_handson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
