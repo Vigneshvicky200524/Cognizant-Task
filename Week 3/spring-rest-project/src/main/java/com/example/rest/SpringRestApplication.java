a448457d5ffe6e8c6861b6ace7cbe55921bd718f
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-09 13:24:21 +0530

    Add files via upload

---
+++ Week 3/spring-rest-project/src/main/java/com/example/rest/SpringRestApplication.java
@@ -0,0 +1,16 @@
+package com.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringRestApplication.class, args);
+
+        System.out.println("Spring REST Application Started");
+    }
+
+}
