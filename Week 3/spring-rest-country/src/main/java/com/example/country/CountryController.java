8509af5d7f1a23787dc9f2897decdb17ef02f8b0
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-12 11:17:45 +0530

    Add files via upload

---
+++ Week 3/spring-rest-country/src/main/java/com/example/country/CountryController.java
@@ -0,0 +1,16 @@
+package com.example.country;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountry() {
+
+        return new Country("IN", "India");
+
+    }
+
+}
---
+++ Week 3/spring-rest-country/src/main/java/com/example/country/SpringRestCountryApplication.java
@@ -0,0 +1,17 @@
+package com.example.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCountryApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringRestCountryApplication.class, args);
+
+        System.out.println("Country REST Service Started");
+
+    }
+
+}
