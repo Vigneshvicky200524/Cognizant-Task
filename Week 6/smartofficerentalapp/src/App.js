d7bc352e9a927d3fe34aed7eca5c4bb3b7ca888e
Vigneshvicky200524 <vickyjashu2418@gmail.com>
2026-07-23 14:36:57 +0530

    Add files via upload

---
+++ Week 6/smartofficerentalapp/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Smart Office Rental</h1>
+      <h2>Office Space Booking System</h2>
+      <p>This is my React application.</p>
+    </div>
+  );
+}
+
+export default App;
